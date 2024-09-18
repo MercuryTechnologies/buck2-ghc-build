6c5ab765bacae1244bb832eb4f3915a569307f9a
Ian-Woo Kim <ianwookim@gmail.com>
2024-09-18 12:06:51 -0700

    First GHC HEAD enabled build using ghcHEAD-toolchains (#1)
    By changing toolchains = ghcHEAD-toolchains in .buckconfig and set $GHC environment variable to a desired compiler under testing, now one can build a project experimentally with GHC HEAD (or whatever version). This plugin is impure (i.e. no guarantee in reproducibility), so it is used at one's own risk.
    
    * ghcHEAD-toolchains
    * dummy _build_package_db
    * ghcHEAD-toolchains restarted from working toolchains. remove nix_build_drv
    * remove need for drv_json
    * further clean up
    * remove further
    * integrate environmental GHC!
    * further clean-up
    * NIX_GHC_PATH -> GHC_PATH, NIX_GHC -> GHC
    * now aeson is considered as builtin
    * remove aeson dep for now. first success with GHC HEAD!
    * remove unnecessary files.

--- flake.nix
+++ flake.nix
@@ -62,17 +62,41 @@
       };
 
       devShells = rec {
-        buck2 = pkgs.mkShellNoCC {
-          name = "buck2-test-shell";
+        buck2 = buck2-nix;
+        buck2-nix = pkgs.mkShellNoCC {
+          name = "buck2-nix-shell";
           packages = buck2BuildInputs ++ [
             pkgs.buck2-source
             pkgs.nix
+            pkgs.jq
           ];
 
           shellHook = ''
-            export PS1="\n[buck2-test-shell:\w]$ \0"
+            export PS1="\n[buck2-nix:\w]$ \0"
           '';
         };
+        buck2-ghcHEAD = pkgs.mkShell {
+          name = "buck2-ghcHEAD-shell";
+          packages = buck2BuildInputs ++ [
+            pkgs.buck2-source
+            pkgs.nix
+            pkgs.jq
+          ];
+          # GHC in invokes Nix cc, cc-wrapper invokes mktemp from $PATH. Also GHC invokes otool and
+          # install_name_tool from $PATH on Darwin.
+          GHC_PATH = pkgs.lib.makeSearchPath "bin" ([ pkgs.coreutils ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.stdenv.cc.bintools
+            pkgs.darwin.cctools
+          ]);
+
+          shellHook = ''
+            # Haskell toolchain
+            #export GHC=${ghcWithPackages}/bin/ghc
+            #
+            export PS1="\n[buck2-ghcHEAD:\w]$ \0"
+          '';
+        };
+
       };
     });
 
